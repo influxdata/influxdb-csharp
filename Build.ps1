9df9e869b4fcf6a6419fec205b5a1372796a2d99
Jared Klopper <spuzzy2@hotmail.com>
2017-05-17 20:38:44 +1200

    Build nuget package the correct dependency version suffix.

--- Build.ps1
+++ Build.ps1
@@ -4,18 +4,18 @@ echo "build: Build started"
 
 Push-Location $PSScriptRoot
 
-if(Test-Path .\artifacts) {
-	echo "build: Cleaning .\artifacts"
-	Remove-Item .\artifacts -Force -Recurse
-}
-
-& dotnet restore --no-cache
-
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{
 	$true = "beta-$revision";
 	$false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+
+if(Test-Path .\artifacts) {
+	echo "build: Cleaning .\artifacts"
+	Remove-Item .\artifacts -Force -Recurse
+}
+
+& dotnet msbuild "/t:Restore" /p:VersionSuffix=$suffix /p:Configuration=Release
 
 echo "build: Version suffix is $suffix"
 
