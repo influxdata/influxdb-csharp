cc19bd67717d7680716d471c66af52fa4cc57271
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-06-21 08:34:10 +1000

    Set up dev branch builds to prepare for a 1.0

--- Build.ps1
+++ Build.ps1
@@ -7,7 +7,7 @@ Push-Location $PSScriptRoot
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{
-	$true = "beta-$revision";
+	$true = "$revision";
 	$false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
 if(Test-Path .\artifacts) {
