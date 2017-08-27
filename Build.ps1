72912106b2dfcd035b2e213b14d77e3fdcf7eda0
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-08-28 09:23:34 +1000

    Try to sort out 1.0 release build

--- Build.ps1
+++ Build.ps1
@@ -1,55 +1,55 @@
 # Originally based on https://github.com/serilog/serilog/blob/dev/Build.ps1 - (c) Serilog Contributors
 
-echo "build: Build started"
-
 Push-Location $PSScriptRoot
-
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{
-	$true = "$revision";
-	$false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
 if(Test-Path .\artifacts) {
 	echo "build: Cleaning .\artifacts"
 	Remove-Item .\artifacts -Force -Recurse
 }
 
-& dotnet msbuild "/t:Restore" /p:VersionSuffix=$suffix /p:Configuration=Release
+& dotnet restore --no-cache
 
-echo "build: Version suffix is $suffix"
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+$commitHash = $(git rev-parse --short HEAD)
+$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
+
+echo "build: Package version suffix is $suffix"
+echo "build: Build version suffix is $buildSuffix" 
 
 foreach ($src in ls src/*) {
-	Push-Location $src
+    Push-Location $src
 
 	echo "build: Packaging project in $src"
 
-	& dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix
-	if($LASTEXITCODE -ne 0) { exit 1 }    
+    & dotnet build -c Release --version-suffix=$buildSuffix
+    & dotnet pack -c Release --include-symbols -o ..\..\artifacts --version-suffix=$suffix --no-build
+    if($LASTEXITCODE -ne 0) { exit 1 }    
 
-	Pop-Location
-}
-
-foreach ($test in ls test/*.PerformanceTests) {
-	Push-Location $test
-
-	echo "build: Building performance test project in $test"
-
-	& dotnet build -c Release
-	if($LASTEXITCODE -ne 0) { exit 2 }
-
-	Pop-Location
+    Pop-Location
 }
 
 foreach ($test in ls test/*.Tests) {
-	Push-Location $test
+    Push-Location $test
 
 	echo "build: Testing project in $test"
 
-	& dotnet test -c Release
-	if($LASTEXITCODE -ne 0) { exit 3 }
+    & dotnet test -c Release
+    if($LASTEXITCODE -ne 0) { exit 3 }
 
-	Pop-Location
+    Pop-Location
+}
+
+foreach ($test in ls test/*.PerformanceTests) {
+    Push-Location $test
+
+	echo "build: Building performance test project in $test"
+
+    & dotnet build -c Release
+    if($LASTEXITCODE -ne 0) { exit 2 }
+
+    Pop-Location
 }
 
 Pop-Location
